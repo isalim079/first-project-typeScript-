3e052dcccda6a66c76cc32ffd5b968539c39c52d
isalim079 <salimreza1025@gmail.com>
2024-05-27 12:33:28 +0600

    custom sendResponse created on utils

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
 
 const createStudent = async (
   req: Request,
@@ -14,9 +16,10 @@ const createStudent = async (
 
     const result = await UserService.createStudentIntoDB(password, studentData);
 
-    res.status(200).json({
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: 'Student is created successfully',
+      message: 'Student created successfully',
       data: result,
     });
   } catch (err) {
