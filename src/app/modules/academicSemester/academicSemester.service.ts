23fc5174b25bb88a08bb7a385b665718b7c9afc1
isalim079 <salimreza1025@gmail.com>
2024-05-29 22:13:35 +0600

    academic semester code validation done. Also all code organized

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,11 +1,18 @@
-import { TAcademicSemester } from "./academicSemester.interface"
-import { academicSemester } from "./academicSemester.model"
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
+import { TAcademicSemester } from './academicSemester.interface';
+import { academicSemester } from './academicSemester.model';
 
-const createAcademicSemesterIntoDB = async(payload: TAcademicSemester) => {
-    const result = await academicSemester.create(payload);
-    return result
-}
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // semester code and name sequence check
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code');
+  }
+
+  const result = await academicSemester.create(payload);
+  return result;
+};
 
 export const AcademicSemesterServices = {
-    createAcademicSemesterIntoDB
-}
+  createAcademicSemesterIntoDB,
+};
