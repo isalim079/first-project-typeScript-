7b0f03252d6ea5cb70be8ce6dde8d458cc7e5a08
isalim079 <salimreza1025@gmail.com>
2024-05-29 19:40:37 +0600

    year type changed to date to string in academic semester

---
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { AcademicSemesterControllers } from './academicSemester.controller'
+import validateRequest from '../../middlewares/validRequest'
+import { AcademicSemesterValidations } from './academicSemester.validation'
+
+const router = express.Router()
+
+router.post('/create-academic-semester', validateRequest(AcademicSemesterValidations.createAcademicSemesterValidationSchema), AcademicSemesterControllers.createAcademicSemester)
+
+export const AcademicSemesterRoutes = router
