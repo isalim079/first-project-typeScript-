ff2b4cc145447d00235472a2a5472d694a08ed0d
isalim079 <salimreza1025@gmail.com>
2024-06-03 19:48:20 +0600

    Academic faculty added

---
+++ src/app/modules/AcademicFaculty/academicFaculty.route.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validRequest';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+const router = express.Router();
+
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    AcademicFacultyValidation.createAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get('/:facultyId', AcademicFacultyControllers.getSingleAcademicFaculty);
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
+
+router.patch(
+  '/:facultyId',
+  validateRequest(
+    AcademicFacultyValidation.updateAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+export const AcademicFacultyRoutes = router
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
-import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.Route';
+import { AcademicFacultyRoutes } from '../modules/AcademicFaculty/academicFaculty.route';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 const router = Router();
 
 const moduleRoutes = [
@@ -16,6 +17,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes
   },
 ];
 
