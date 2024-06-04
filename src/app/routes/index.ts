7b15446f27ac8fac8a9587cff0ab0c8ac069c1e4
isalim079 <salimreza1025@gmail.com>
2024-06-04 11:13:55 +0600

    academic department routes created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
 import { AcademicFacultyRoutes } from '../modules/AcademicFaculty/academicFaculty.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 const router = Router();
 
 const moduleRoutes = [
@@ -21,6 +22,10 @@ const moduleRoutes = [
   {
     path: '/academic-faculties',
     route: AcademicFacultyRoutes
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes
   },
 ];
 
