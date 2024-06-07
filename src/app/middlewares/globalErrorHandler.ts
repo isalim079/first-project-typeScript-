50a5b6206b5072fed2267478e8e1e3c59273595c
isalim079 <salimreza1025@gmail.com>
2024-06-07 16:34:49 +0600

    global error handler organized and make stack error for only development. Hide stack errors for others due to vulnarability of site

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,45 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
+import handleZodError from '../errors/handleZodError';
 
-const globalErrorHandler = (
+const globalErrorHandler : ErrorRequestHandler = (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+ 
+
+  let errorSources: TErrorSource = [{
+    path: '',
+    message: 'Something went wrong!',
+  }]
+
+  
+
+  if(err instanceof ZodError) {
+
+    const simplifiedError = handleZodError(err)
+
+    statusCode = simplifiedError.statusCode
+    message= simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
+
+  }
+
   return res.status(statusCode).json({
     success: false,
     message: message,
-    error: err,
+    errorSources,
+    stack: config.node_env === 'development' ?  err?.stack : null
   });
 };
 
